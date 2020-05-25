56ef14c6f0f9cc70253cc237a30240b6f6370f43
Keith Staines <keith27staines@icloud.com>
2020-05-25 14:09:14 +0100

    Initial Commit

---
+++ KSQuadTree/KSQuadTree.h
@@ -0,0 +1,19 @@
+//
+//  KSQuadTree.h
+//  KSQuadTree
+//
+//  Created by Keith Staines on 25/05/2020.
+//  Copyright © 2020 Keith Staines. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSQuadTree.
+FOUNDATION_EXPORT double KSQuadTreeVersionNumber;
+
+//! Project version string for KSQuadTree.
+FOUNDATION_EXPORT const unsigned char KSQuadTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSQuadTree/PublicHeader.h>
+
+
